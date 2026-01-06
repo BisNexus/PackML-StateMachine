5dfb3a7a513ba9cd47d85cf008572a17c132811d
Priyanku <priyankubiswas85@gmail.com>
2026-01-06 01:12:16 +0100

    test without yield

--- PackML-StateMachine.Tests/transitioning/TestStateMachineSetup.cs
+++ PackML-StateMachine.Tests/transitioning/TestStateMachineSetup.cs
@@ -5,24 +5,21 @@ namespace PackML_StateMachine.Tests.transitioning;
 
 public class TestStateMachineSetup
 {
-    private readonly int dummyActionTime = 500;
-    private readonly IStateAction dummyAction;
+    const int dummyActionTime = 500;
+    IStateAction dummyAction = new DummyAction(dummyActionTime);
 
-    public TestStateMachineSetup()
-    {
-        dummyAction = new DummyAction(dummyActionTime);
-    }
-
-    public static IEnumerable<object[]> GetActiveStateNames()
-    {
-        foreach (ActiveStateName stateName in Enum.GetValues<ActiveStateName>())
-        {
-            yield return new object[] { stateName };
-        }
-    }
-
-    
-    [MemberData(nameof(GetActiveStateNames))]
+    [Theory]
+    [InlineData(ActiveStateName.Starting)]
+    [InlineData(ActiveStateName.Execute)]
+    [InlineData(ActiveStateName.Completing)]
+    [InlineData(ActiveStateName.Holding)]
+    [InlineData(ActiveStateName.Unholding)]
+    [InlineData(ActiveStateName.Suspending)]
+    [InlineData(ActiveStateName.Unsuspending)]
+    [InlineData(ActiveStateName.Stopping)]
+    [InlineData(ActiveStateName.Clearing)]
+    [InlineData(ActiveStateName.Aborting)]
+    [InlineData(ActiveStateName.Resetting)]
     public void testActionSetup(ActiveStateName stateName)
     {
         // Arrange
